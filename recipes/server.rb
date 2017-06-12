7ad5b1da2edfffbde3f92461006c14059abd5d6a
chef <chef@centos-6.(none)>
2017-06-12 19:39:22 +0200

    Usamos una plantilla .ERB

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,13 +2,10 @@ package 'httpd' do
 	action :install
 end
 
-file '/var/www/html/index.html' do
-	content "<h1> Hola Chef</> \n
-
-        <h2>ipaddress:#{node['ipaddress']}</h2>
-
-        "
-	action :create
+        template '/var/www/html/index.html' do
+	
+        source 'index.html.erb'
+        action :create
 end	
 
 service 'httpd' do
